2de692487495e02c6b00067166c659845076b56d
MATVEY <75066032+MATVEY-Git-Hub@users.noreply.github.com>
2023-05-24 08:31:08 +0300

    Update popup.js

--- popup.js
+++ popup.js
@@ -5,12 +5,29 @@ const inputField2 = document.getElementById('myInput4'); //неверный ко
 const buttons = document.querySelectorAll('.clipboard-btn');
 
 function copy() {
-  let stroke = `Приказы:\n`
-  console.log(inputField1.value, inputField2.value, inputFieldS.value)
-
   const orderOffset = Number(inputFieldS.value); // смещение
   let badOrderNumber = Number(inputField1.value); // номер первого неправильного приказа
   let badFinishOrderNumber = Number(inputField2.value); // номер последнего неправильного приказа
+
+  if (badFinishOrderNumber < badOrderNumber) {
+    Toastify({
+      text: 'Как последний приказ может быть меньше первого?',
+      duration: 3000,
+      newWindow: true,
+      close: true,
+      gravity: "top", // `top` or `bottom`
+      position: "left", // `left`, `center` or `right`
+      stopOnFocus: true, // Prevents dismissing of toast on hover
+      style: {
+        background: "linear-gradient(to right, #cc2e55, #cc2e55)",
+      },
+      onClick: function () { } // Callback after click
+    }).showToast();
+    return;
+  }
+
+  let stroke = `Приказы:\n`
+  console.log(inputField1.value, inputField2.value, inputFieldS.value)
 
   while (badOrderNumber < badFinishOrderNumber + 1) {
     stroke += `#${badOrderNumber} -> #${badOrderNumber + orderOffset}\n`;
@@ -18,7 +35,6 @@ function copy() {
   }
   console.log(stroke)
   navigator.clipboard.writeText(stroke);
-
   Toastify({
     text: "Приказы скопированы!",
     duration: 3000,
